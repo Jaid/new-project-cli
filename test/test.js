e501a4cc197b24a6d11e1040a6578c10500df575
Jaid <jaid.jsx@gmail.com>
2019-07-08 10:19:40 +0200

    Added higher timeout for test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 import path from "path"
 
 import coffee from "coffee"
+import ms from "ms.macro"
 
 const main = path.resolve(process.env.MAIN)
 
 it("should run", () => coffee.fork(main, ["A"])
   .expect("code", 1)
   .debug(true)
-  .end())
+  .end(), ms`30 seconds`)
